5cd3f4e5cc83b48da95516ad50dc682ef3e105d0
fernando <falvarezz139@gmail.com>
2025-03-16 20:45:29 +0100

    ejercicio 5.17

--- tests/blog-app.spec.js
+++ tests/blog-app.spec.js
@@ -2,17 +2,45 @@ const { test, expect, beforeEach, describe } = require('@playwright/test')
 
 describe('Blog app', () => {
   beforeEach(async ({ page }) => {
-    // Navegar a la URL de la aplicación
+
     await page.goto('http://127.0.0.1:5173')
   })
 
   test('Login form is shown', async ({ page }) => {
-    // Verificar que el formulario de inicio de sesión esté visible
-    const loginForm = await page.locator('form#login-form'); // Usa el selector adecuado para tu formulario
+
+    const loginForm = await page.locator('form#login-form');
     await expect(loginForm).toBeVisible();
 
-    // Verificar si el campo de usuario está presente
-    const usernameField = await page.locator('input[name="username"]'); // Usa el selector adecuado para el campo de usuario
+    const usernameField = await page.locator('input[name="username"]');
     await expect(usernameField).toBeVisible();
+  })
+
+  describe('Login', () => {
+    test('succeeds with correct credentials', async ({ page }) => {
+
+      const usernameField = await page.locator('input[name="username"]');
+      const passwordField = await page.locator('input[name="password"]');
+      const loginButton = await page.locator('button[type="submit"]');
+
+
+      await usernameField.fill('fernando');
+      await passwordField.fill('correct-password');
+      await loginButton.click();
+
+      await expect(page).toHaveURL('http://127.0.0.1:5173/dashboard'); 
+    })
+
+    test('fails with wrong credentials', async ({ page }) => {
+      const usernameField = await page.locator('input[name="username"]');
+      const passwordField = await page.locator('input[name="password"]');
+      const loginButton = await page.locator('button[type="submit"]');
+
+      await usernameField.fill('fernando');
+      await passwordField.fill('wrong-password');
+      await loginButton.click();
+
+      const errorMessage = await page.locator('div#error-message');
+      await expect(errorMessage).toBeVisible();
+    })
   })
 })
